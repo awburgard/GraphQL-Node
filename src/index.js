65ba24625a4c7f400edbdd787ea322d229abedab
awburgard <awburgard@gmail.com>
2020-02-02 08:27:54 -0600

    moves schema to its own file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
-type Link {
-    id: ID!
-    description: Sting!
-    url: String!
-}
-`
-
+let idCount = links.length
 const resolvers = {
     Query: {
-        info: () => null
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        },
     }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
 })
 
